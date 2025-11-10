3c137a22fb2cea65917a3bbed531bf6ee27d2a49
Ali haider <alihaidergr8@gmail.com>
2025-11-10 23:59:16 +0500

    task 3: completed

--- Task3/app.js
+++ Task3/app.js
@@ -1,1 +1,11 @@
-console.log("Task 3 started")
+console.log(`your current node version is ${process.version}`);
+console.log(`your current directory is ${process.cwd()}`);
+
+console.log(__filename);
+console.log(__dirname);
+
+const argv = process.argv[2];
+
+console.log(`hello ${argv}`);
+
+
